4f91cbf2cb277c90d07322c4cceddfc8357a45fd
kavin1022 <pinghangfan1022@gmail.com>
2021-08-15 22:05:10 +1200

    Add project files.

---
+++ Model/data.cs
@@ -0,0 +1,8 @@
+using System.ComponentModel.DataAnnotations;
+public class Customer
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+}
